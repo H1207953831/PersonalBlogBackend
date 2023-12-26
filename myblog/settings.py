2fd9ea88c21a87723deb411ade58412bde001f50
huangzj_pikachu <1207953831@qq.com>
2023-12-26 16:04:00 +0800

    依赖环境

--- myblog/settings.py
+++ myblog/settings.py
@@ -119,9 +119,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_DIR = [os.path.join(BASE_DIR, 'static')]
+STATIC_DIR = [os.path.join(BASE_DIR, 'frontend/dist/')]
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'collected_static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
